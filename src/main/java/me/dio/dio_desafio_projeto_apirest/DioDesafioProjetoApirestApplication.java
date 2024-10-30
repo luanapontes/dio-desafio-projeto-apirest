fc9eb78fc85673b410075fab3682ecf23caf1c7e
luana <luana.xavier@dcx.ufpb.br>
2024-10-30 13:19:23 -0300

    estrutura do projeto

---
+++ src/main/java/me/dio/dio_desafio_projeto_apirest/DioDesafioProjetoApirestApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_projeto_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioProjetoApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioProjetoApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/dio_desafio_projeto_apirest/DioDesafioProjetoApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_projeto_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioProjetoApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
